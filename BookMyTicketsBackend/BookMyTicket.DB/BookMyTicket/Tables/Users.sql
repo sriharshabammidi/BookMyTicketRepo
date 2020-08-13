67f15d02c76ced91b2a9ca6227d4eddf5b6f19a4
Sri Harsha Bammidi <sriharshabammidi@outlook.com>
2020-08-13 19:19:22 +0530

    Added constraint in User table

--- BookMyTicketsBackend/BookMyTicket.DB/BookMyTicket/Tables/Users.sql
+++ BookMyTicketsBackend/BookMyTicket.DB/BookMyTicket/Tables/Users.sql
@@ -4,6 +4,9 @@
     [LastName]  VARCHAR (50) NULL,
     [Email]     VARCHAR (50) NOT NULL,
     [Password]  VARCHAR (50) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [constraint_email] UNIQUE NONCLUSTERED ([Email] ASC)
 );
+
+
 
